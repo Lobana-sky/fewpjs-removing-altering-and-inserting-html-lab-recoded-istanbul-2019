7df01b1d4a9315796c6e9a31a320c37b6d0b98ca
lobana alarabi <loubanaeng@gmail.com>
2020-01-20 22:39:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,8 @@
 let element=document.createElement('div');
 document.body.appendChild(element);
 let ul=document.createElement('ul');
-
+for(let i=0;i<3;i++)
+{
+  let li=document.createElement('li');
+  
+}
